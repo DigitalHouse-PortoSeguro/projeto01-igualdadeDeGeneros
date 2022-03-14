34a47ef5477dcf0948633af5927848ba6fcaeaca
Adriana Mucciolo <drishaolin@gmail.com>
2022-03-13 22:25:33 -0300

    alterações de código feitas pelo Wesley

--- src/igualdadeDeGeneros/Participante.java
+++ src/igualdadeDeGeneros/Participante.java
@@ -4,26 +4,40 @@ package igualdadeDeGeneros;
 import java.util.Scanner;
 
 public class Participante {
-	
-	public static void CadastraParticipante() {
-
+		
 		String genero;
 		String filhos;
 		String numeroParticipantes;
-	
+		boolean participanteCadastrado=true;
+		
+		public static void CadastraParticipante() {
+
+			String genero;
+			String filhos;
+			String numeroParticipantes;
+			boolean participanteCadastrado;
+			
 		Scanner leia = new Scanner(System.in);
 	
-		System.out.println("\n Olá! Bora testar como está dividida a carga mental na sua casa?!");
-		System.out.println("\n Para começarmos vamos precisar de alguns dados! \n Qual o seu gênero? (masculino/feminino)");
-		genero= leia.nextLine();
-		System.out.println("\n Tem filho(s)? (sim/não)");
-		filhos = leia.nextLine();
-		System.out.println("\n Vai jogar em dupla? (sim/não)");
-		numeroParticipantes= leia.nextLine();
+		System.out.println("\n Ol�! Bora testar como est� dividida a carga mental na sua casa?"
+				+ "\nMas antes de iniciar, Mas antes de iniciar precisamos fazer o seu cadastro!");
 		
-		/* Se o participante for do sexo masculino será uma serie de perguntas e se for do feminino outra? 
-		Ou pontuação diferente para cada um dos sexos?
-		 se tem filhos uma serie de perguntas? ou apenas anular aquela questão com não se aplica?*/
+		System.out.println("\n Siga os passos abaixo para se cadastrar:"
+				+ "\nQual o seu g�nero? (M) Masculino ou (F) Feminino");
+		genero = leia.nextLine();
+		genero = genero.toUpperCase();
+		
+		System.out.println("\n Tem filho(s)? (S) Sim ou (N) N�o");
+		filhos = leia.nextLine();
+		filhos = filhos.toUpperCase();
+		
+		System.out.println("\n Vai jogar em dupla? (S) Sim ou (N) N�o");
+		numeroParticipantes= leia.nextLine();
+		numeroParticipantes = numeroParticipantes.toUpperCase();
+		
+		
+		
+		
 		
 		
 	}
--- src/igualdadeDeGeneros/appMain.java
+++ src/igualdadeDeGeneros/appMain.java
@@ -1,17 +1,94 @@
 package igualdadeDeGeneros;
 
+import java.util.Scanner;
 
 public class appMain {
 
 	public static void main(String[] args) {
 		
-		Participante part = new Participante();
 		//Participante();
 		//PopulaPerguntas();
 		//Quiz
 		//Resultado
 
-		System.out.println("\n Olá! Bora testar como está dividida a carga mental na sua casa?!");
+		Participante part = new Participante();
+		PopulaPerguntas pp = new PopulaPerguntas();
+				
+		Scanner ler = new Scanner(System.in);
+		
+		Participante.CadastraParticipante();//Chamando Método CadastraParticipante
+		
+		if(part.participanteCadastrado==true)/* Se participanteCadastrado==true linha 11 da Classe Participante, ou seja 
+												se o participante ja for cadastrado ele vai para o Menu Principal, mas
+												 se for false ele tem que fazer o cadastro, para conseguir jogar.*/
+		{
+		
+		int op=100;
+				
+		do
+		{
+			switch(op)
+			{
+			case 100://Menu Principal
+			{
+				System.out.println("\nEscolha uma das opções:"
+							      // +"\n(100) Menu Principal"
+								   +"\n(1) Quiz Masculino"
+								   +"\n(2) Quiz Feminino"
+								   +"\n(3) Quiz Casal 1"
+								   +"\n(4) Quiz Casal 2"
+								   +"\n(0) Sair");
+				op = ler.nextInt();
+				break;
+			}
+			case 1://Quiz Masculino
+			{
+				ler.nextLine();
+				op=pp.perguntasMasculino1(op);/*Quando o Método perguntasMasculino1 for finalizado, o mesmo retornara 100 *return 100* 
+												   linha 128 da PopulaPerguntas. Que será atribuido a variavel op da Classe appMain... 
+												   e quando 'op = 100' o mesmo direciona para o Menu Principal.*/
+				break;
+			}
+			case 2: //Quiz Feminino
+			{
+				ler.nextLine();
+				op=pp.perguntasFeminino1(op);/*Quando o Método perguntasFeminino1 for finalizado, o mesmo retornara 100 *return 100* 
+											   linha 205 da PopulaPerguntas. Que será atribuido a variavel op da Classe appMain... 
+											   e quando 'op = 100' o mesmo direciona para o Menu Principal.*/
+				break;
+			}
+			case 3: //Quiz Casal 1
+			{
+				ler.nextLine();
+				op=pp.perguntasCasal1(op);/*Quando o Método perguntasFeminino1 for finalizado, o mesmo retornara 100 *return 100* 
+										   linha 346 da PopulaPerguntas. Que será atribuido a variavel op da Classe appMain... 
+										   e quando 'op = 100' o mesmo direciona para o Menu Principal.*/
+				break;
+			}
+			case 4://QuizTeste Casal 2
+			{
+				ler.nextLine();
+				op=pp.perguntasCasal2(op);/*Quando o Método perguntasFeminino1 for finalizado, o mesmo retornara 100 *return 100* 
+										   linha 453 da PopulaPerguntas. Que será atribuido a variavel op da Classe appMain... 
+										   e quando 'op = 100' o mesmo direciona para o Menu Principal.*/
+				break;
+			}
+			
+			case 0://Para Sair do Programa
+			{
+				ler.nextLine();
+				break;
+			}
+				
+			}
+						
+		}
+		while(op!=0);//Se for igual a zero, termina/finaliza o programa
+		
+		System.out.println("\n Obrigado por se tornar uma pessoa cada vez melhor! Cotinue sempre assim!");
+		}
 	}
 
-}
+	}
+
+
